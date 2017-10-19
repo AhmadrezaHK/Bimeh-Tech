3cd65f5c1a957d1158c2df28c3b0bef7cb80c845
AhmadrezaHK <rahnama378@gmail.com>
2017-10-20 00:46:43 +0330

    addcar template

---
+++ src/components/Pages/BeforeAddCar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="addCar">
+    <div class="img"></div>
+    <p>هیج خودرویی در پروفایل خود ندارید.</p>
+    <app-button button_label="افزودن خودروی جدید"></app-button>
+  </div>
+</template>
+
+<script>
+  import Button from '@/components/Elements/Button.vue'
+
+  export default {
+    components:{
+      'app-button':Button
+    }
+  }
+</script>
+
+<style scoped>
+  .img{
+    background: url("../../assets/img/car.png") no-repeat center;
+    background-size: contain;
+    height: 200px;
+  }
+</style>
--- src/components/Pages/Dashboard.vue
+++ src/components/Pages/Dashboard.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="dashboard">
-    <app-add-car></app-add-car>
+    <!--<app-add-car></app-add-car>-->
+    <!--<app-before-add-car></app-before-add-car>-->
   </div>
 </template>
 
 <<script>
 import store from '@/vuexstore'
 import addCar from './AddCar.vue'
+import beforeAddCar from './BeforeAddCar.vue'
 
 export default {
   components:{
-    'app-add-car':addCar
+    'app-add-car':addCar,
+    'app-before-add-car':beforeAddCar
   }
 }
 </script>
